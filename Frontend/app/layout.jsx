96c21810fa2f3299dd72aeae73fb9af33b4358db
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-12 19:24:46 +0530

    Refactor favicon and shortcut icon configuration in layout.jsx

--- Frontend/app/layout.jsx
+++ Frontend/app/layout.jsx
@@ -6,17 +6,15 @@ const lato = Lato({ subsets: ['latin'], weight: ['100', '300', '400', '700'] })
 export const metadata = {
 	title: "Spark Chat",
 	description: "Real-Time Chat Application",
+	icons: {
+		icon: "/icons/favicon.ico",
+		shortcut: "/icons/favicon.png",
+	},
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" encoding="UTF-8">
-			<head>
-				<link rel="icon" href="/icons/favicon.png" sizes="any" />
-				<link rel="shortcut icon" href="/icons/favicon.ico" sizes="any" />
-				<link rel="icon" type="image/x-icon" href="/icons/favicon.ico" sizes="any" />
-				<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" sizes="any" />
-			</head>
 			<body className={`${lato.className} antialiased`}>
 				{children}
 			</body>
