71e7acd81c994fde2d8151444e75e99c4cf5d365
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-13 01:11:27 +0530

    Refactor WebSocket server and update landing page UI

--- Frontend/services/index.js
+++ Frontend/services/index.js
@@ -1,25 +1,14 @@
-// Use the native WebSocket API to connect to the Ballerina WebSocket server
-const socket = new WebSocket('ws://localhost:21003/ws');
-
-// Log connection status
-socket.onopen = () => {
-	console.log("Connected to WebSocket servear");
-	socket.send(socket.protocol);
-};
-
-// Handle messages received from the server
-socket.onmessage = (event) => {
-	console.log("Message from server: ", event.data);
-};
-
-// Handle connection close
-socket.onclose = () => {
-	console.log("Connection closed");
-};
-
-// Handle any errors
-socket.onerror = (error) => {
-	console.error("WebSocket error: ", error);
-};
-
-export default socket;
+fetch("http://localhost:8080/hsrequest", headers = {
+	method: "GET",
+	headers: {
+		"Content-Type": "application/json",
+		"Upgrade": "websocket",
+		"Connection": "upgrade",
+		"Sec-WebSocket-Key": "dGhlIHNhbXBsZSBub25jZQ==",
+		"Sec-WebSocket-Version": "13"
+	}
+}).then(response => {
+	console.log(response);
+}).catch(error => {
+	console.log(error);
+});
