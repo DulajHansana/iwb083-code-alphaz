40675cbe6c3f33b45e8b6725dd7008254aeef90d
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-12 15:17:58 +0530

    Refactor layout components and update dependencies

---
+++ Frontend/app/(auth)/sign-up/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+	return (
+		<div>Sign up form here</div>
+	)
+}
+
+export default SignUp
---
+++ Frontend/app/(root)/profile/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+	<div>Profile page here</div>
+  )
+}
+
+export default Profile
