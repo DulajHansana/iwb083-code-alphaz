c8b55499164e9715602a5c63df82c4e31da76858
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-12 18:47:06 +0530

    Update next.config.mjs: Set basePath and assetPrefix for GitHub Pages

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
 	output: "export",
 	// ? basepath and assetPrefix will be used only for github pages
-	// basePath: '/iwb083-code-alphaz',
-	// assetPrefix: '/iwb083-code-alphaz',
+	basePath: '/iwb083-code-alphaz',
+	assetPrefix: '/iwb083-code-alphaz',
 };
 
 export default nextConfig;
