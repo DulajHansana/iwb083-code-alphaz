8e12ea6a03407257a7ea2f055ee9e90325804928
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-12 18:45:56 +0530

    Update next.config.mjs and readme.md

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	basePath: '/iwb083-code-alphaz',
-	assetPrefix: '/iwb083-code-alphaz',
+	// ? basepath and assetPrefix will be used only for github pages
+	// basePath: '/iwb083-code-alphaz',
+	// assetPrefix: '/iwb083-code-alphaz',
 };
 
 export default nextConfig;
