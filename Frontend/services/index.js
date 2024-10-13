61d0ec387fb5f237e7dc856d2caf29c51f9449de
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-13 11:54:57 +0530

    Refactor WebSocket server, update landing page UI, and add JWT token authorization

--- Frontend/services/index.js
+++ Frontend/services/index.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 export const socket = new WebSocket(null);
 
 socket.addEventListener("open", () => {
@@ -15,3 +17,29 @@ socket.addEventListener("close", () => {
 socket.addEventListener("error", (error) => {
 	console.error("WebSocket error:", error);
 });
+
+export const connect = async () => {
+	await fetch("http://localhost:8080/authorize", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + jwtTokenGenerator()
+		}
+	}).then((response) => {
+		if (response.Accepted) {
+			socket.send("Hello, server!");
+		}
+	})
+}
+
+function jwtTokenGenerator() {
+	const payload = {
+		username: "Someone",
+		id: 1,
+		email: "Q5TJt@example.com",
+	};
+
+	const jwToken = jwt.sign(payload, process.env.JWT_SECRET);
+
+	return jwToken;
+}
